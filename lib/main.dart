787f24b7fd06461ecc1d78465673951bd8951856
SebastianJL <johannes.lade@yahoo.com>
2020-09-14 00:08:17 +0200

    Add about dialog with reference to Icons8.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:consumption_tracker/src/charts_tab.dart';
 
+const appName = 'ConsumptionTracker';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(ConsumptionTracker());
@@ -18,7 +20,6 @@ class ConsumptionTracker extends StatelessWidget {
     return FutureBuilder(
         future: _firebaseInit,
         builder: (context, snapshot) {
-
           if (snapshot.connectionState == ConnectionState.done) {
             return MaterialApp(
               theme: ThemeData(primaryColor: Colors.pinkAccent),
@@ -30,7 +31,6 @@ class ConsumptionTracker extends StatelessWidget {
           }
 
           return MaterialApp(home: Center(child: Text('Loading')));
-
         });
   }
 }
@@ -41,8 +41,36 @@ class HomePage extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              Container(
+                color: Theme.of(context).primaryColor,
+                child: DrawerHeader(
+                  child: Text(
+                    appName,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  ),
+                ),
+              ),
+              AboutListTile(
+                child: Text('About'),
+                icon: Icon(Icons.info_outline),
+                applicationName: appName,
+                dense: false,
+                applicationIcon: Image.asset("assets/icons8-odometer-50.png"),
+                aboutBoxChildren: [
+                  Text('App icon provided by Icons8, see https://icons8.com')
+                ],
+              )
+            ],
+          ),
+        ),
         appBar: AppBar(
-          title: Text('ConsumptionTracker'),
+          title: Text('BMW R1100R'),
           bottom: TabBar(tabs: [
             Tab(text: 'Overview'),
             Tab(text: 'Charts'),

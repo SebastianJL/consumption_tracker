9557c09b7828b8d763044b29f812937c17a59220
SebastianJL <johannes.lade@yahoo.com>
2020-09-15 14:59:28 +0200

    Extract Drawer into its own Widget.

--- lib/main.dart
+++ lib/main.dart
@@ -41,53 +41,7 @@ class HomePage extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              Container(
-                color: Theme.of(context).primaryColor,
-                child: DrawerHeader(
-                  child: Column(
-                    children: [
-                      Expanded(
-                          child: Image.asset(
-                              'assets/icons8-odometer-white-100.png')),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          appName,
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Theme.of(context).colorScheme.onPrimary,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.motorcycle),
-                title: Text('Vehicle 1'),
-              ),
-              ListTile(
-                leading: Icon(Icons.directions_car),
-                title: Text('Vehicle 2'),
-              ),
-              Divider(),
-              AboutListTile(
-                child: Text('About'),
-                icon: Icon(Icons.info_outline),
-                applicationName: appName,
-                dense: false,
-                applicationIcon: Image.asset("assets/icons8-odometer-50.png"),
-                aboutBoxChildren: [
-                  Text('App icon provided by Icons8, see https://icons8.com')
-                ],
-              )
-            ],
-          ),
-        ),
+        drawer: AppDrawer(),
         appBar: AppBar(
           title: Text('BMW R1100R'),
           bottom: TabBar(tabs: [
@@ -101,6 +55,63 @@ class HomePage extends StatelessWidget {
             ChartsTab(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: DrawerHeader(
+              child: Column(
+                children: [
+                  Expanded(
+                      child: Image.asset(
+                          'assets/icons8-odometer-white-100.png')),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      appName,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Theme.of(context).colorScheme.onPrimary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.motorcycle),
+            title: Text('Vehicle 1'),
+          ),
+          ListTile(
+            leading: Icon(Icons.directions_car),
+            title: Text('Vehicle 2'),
+          ),
+          Divider(),
+          AboutListTile(
+            child: Text('About'),
+            icon: Icon(Icons.info_outline),
+            applicationName: appName,
+            dense: false,
+            applicationIcon: Image.asset("assets/icons8-odometer-50.png"),
+            aboutBoxChildren: [
+              Text('App icon provided by Icons8, see https://icons8.com')
+            ],
+          )
+        ],
       ),
     );
   }
